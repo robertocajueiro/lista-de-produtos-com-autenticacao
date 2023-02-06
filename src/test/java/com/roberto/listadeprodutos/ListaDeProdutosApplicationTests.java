f6c0f777dca273c1de180da8f8aced341299b766
robertocajueiro <beto.cajueiro@gmail.com>
2023-02-06 17:22:52 -0300

    first commit

---
+++ src/test/java/com/roberto/listadeprodutos/ListaDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.listadeprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
