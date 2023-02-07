922b10071a52abc848a6fc9f9e51d0544d15439f
robertocajueiro <beto.cajueiro@gmail.com>
2023-02-07 18:10:54 -0300

    Correção no nome da entidade usuario e criação script sql usuario

--- src/main/java/com/roberto/listadeprodutos/domain/model/Usuario.java
+++ src/main/java/com/roberto/listadeprodutos/domain/model/Usuario.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
+@Table(name = "tbl_usuario")
 public class Usuario {
 	
 	@Id
